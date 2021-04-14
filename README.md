0caf80fffcad90bc07ec4c9faff4cd21b777cdab
Sergey Arefiev <info@areh.ru>
2021-04-14 05:23:12 +0500

    Source & readme

---
+++ README.md
@@ -0,0 +1,16 @@
+# DocConverter
+DocConverter - simple web-api document converter based on single threaded LibreOffice runner
+
+## Requirements
+[LibreOffice](https://www.libreoffice.org/) should be installed
+
+## Defaults
+Listening port - 8080. To change, use `--server.port=80`
+
+Listening address - localhost. To change, use `--server.address=192.168.1.1`
+
+Timeout to wait LibreOffice process - 120 seconds. To change, use `FILE_CONVERT_TIMEOUT` environment variable
+
+## Usage
+- `/status/` GET request returns OK if service is up and running
+- `/convert/` POST (with DOC \ XLS file content in body) request returns PDF in response body.
---
+++ src/main/java/ru/areh/DocConverter/ConverterController.java
@@ -0,0 +1,90 @@
+package ru.areh.DocConverter;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+@RestController
+public class ConverterController {
+
+    private static final int fileConvertTimeout;
+
+
+    private static int parseInt(String value, int defaultValue) {
+        try {
+            return Integer.parseInt(value);
+        } catch (NumberFormatException e) {
+            return defaultValue;
+        }
+    }
+
+
+    static {
+        fileConvertTimeout = parseInt(System.getenv("FILE_CONVERT_TIMEOUT"), 120);
+    }
+
+
+    @GetMapping("/status/")
+    public ResponseEntity<String> status() {
+        return new ResponseEntity<>("OK", HttpStatus.OK);
+    }
+
+
+    @PostMapping("/convert/")
+    public ResponseEntity<Object> convert(@RequestBody byte[] requestBody) {
+        if (requestBody == null || requestBody.length == 0)
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+
+        try {
+            var resultFile = ConvertFile(requestBody);
+
+            if (resultFile == null)
+                return new ResponseEntity<>(HttpStatus.UNSUPPORTED_MEDIA_TYPE);
+
+            var responseBody = Files.readAllBytes(resultFile);
+            Files.delete(resultFile);
+            return new ResponseEntity<>(responseBody, HttpStatus.OK);
+
+        } catch (IOException | InterruptedException e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
+    private Path ConvertFile(byte[] originalFileContent) throws IOException, InterruptedException {
+        var tempPathName = "temp";
+        var tempPath = Paths.get(tempPathName);
+        if (!Files.isDirectory(tempPath)) {
+            Files.createDirectories(tempPath);
+        }
+
+        var baseFileName = Paths.get(tempPath.toAbsolutePath().toString(), UUID.randomUUID().toString());
+        var originalFile = Paths.get(baseFileName + ".request");
+        var pdfFile = Paths.get(baseFileName + ".pdf");
+        Files.write(originalFile, originalFileContent);
+
+        var convertResult = startConverter(tempPath, originalFile);
+        Files.delete(originalFile);
+        return convertResult ? pdfFile : null;
+    }
+
+
+    private synchronized boolean startConverter(Path tempPath, Path originalFile) throws IOException, InterruptedException {
+        var processBuilder = new ProcessBuilder(
+                "libreoffice",
+                "--headless",
+                "--convert-to", "pdf",
+                "--outdir", tempPath.toAbsolutePath().toString(),
+                originalFile.toString());
+
+        var officeProcess = processBuilder.start();
+        return officeProcess.waitFor(fileConvertTimeout, TimeUnit.SECONDS);
+    }
+}
